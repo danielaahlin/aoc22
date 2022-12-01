24b23922300b3c798081810eb1b4ccbac5438209
Daniel Åhlin <daniel.ahlin96@gmail.com>
2022-12-01 06:14:06 +0100

    feat: Day 1 completed

--- 01/index.js
+++ 01/index.js
@@ -1,15 +1,29 @@
 const { readFile } = require('../filereader')
 
 const problem1 = (indata) => {
-  console.log(indata)
+  const elfCalories = {}
+  let elfNbr = 0
+
+  indata.forEach((dp) => {
+    if (dp === '') {
+      elfNbr += 1
+      elfCalories[elfNbr] = 0
+    } else {
+      elfCalories[elfNbr] += Number(dp)
+    }
+  })
+
+  return Object.values(elfCalories).sort((a, b) => (a < b ? 1 : -1))
 }
 
 const problem2 = (indata) => {
-  console.log(indata)
+  const data = problem1(indata)
+
+  return data.slice(0, 3).reduce((a, b) => a + b)
 }
 
 const main = (test) => {
-  const paths = ['./test-input.txt', './01input.txt', './02input.txt']
+  const paths = ['./test-input.txt', './01input.txt']
 
   if (test) {
     console.log(' --- TEST INPUT ---')
@@ -26,8 +40,8 @@ const main = (test) => {
   } else {
     console.log('Solution to problem 1: ', problem1(readFile(paths[1])))
     console.log(' --- ')
-    console.log('Solution to problem 2: ', problem2(readFile(paths[2])))
+    console.log('Solution to problem 2: ', problem2(readFile(paths[1])))
   }
 }
 
-main(true)
+main()
