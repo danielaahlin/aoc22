55e86e76ce22f4daa0900cf64e51d2c9e73a918d
Daniel Åhlin <daniel.ahlin96@gmail.com>
2022-12-03 06:54:40 +0100

    Day 02 completed

--- 02/index.js
+++ 02/index.js
@@ -1,11 +1,81 @@
 const { readFile } = require('../filereader')
 
+/*
+    A - X - rock
+    B - Y - paper
+    C - Z - scissor 
+  */
+
 const problem1 = (indata) => {
-  return indata
+  const rpsPoints = {
+    X: 1,
+    Y: 2,
+    Z: 3
+  }
+  const outcome = {
+    X: {
+      A: 3, //'D',
+      B: 0, // L,
+      C: 6 //'W'
+    },
+    Y: {
+      A: 6, //'W',
+      B: 3, //'D',
+      C: 0 // L
+    },
+    Z: {
+      A: 0, // L,
+      B: 6, //'W',
+      C: 3 //'D'
+    }
+  }
+
+  const allRounds = indata.map((round) => {
+    const [opponent, you] = round.split(' ')
+
+    return rpsPoints[you] + outcome[you][opponent]
+  })
+
+  return allRounds.reduce((a, b) => a + b)
 }
 
 const problem2 = (indata) => {
-  return indata
+  const rpsPoints = {
+    A: 1,
+    B: 2,
+    C: 3
+  }
+  const outcomePoints = {
+    X: 0,
+    Y: 3,
+    Z: 6
+  }
+
+  const outcome = {
+    Z: {
+      A: 'B',
+      B: 'C',
+      C: 'A'
+    },
+    Y: {
+      A: 'A',
+      B: 'B',
+      C: 'C'
+    },
+    X: {
+      A: 'C',
+      B: 'A',
+      C: 'B'
+    }
+  }
+
+  const allRounds = indata.map((round) => {
+    const [opponent, you] = round.split(' ')
+
+    return rpsPoints[outcome[you][opponent]] + outcomePoints[you]
+  })
+
+  return allRounds.reduce((a, b) => a + b)
 }
 
 const main = (test) => {
